4f70fcd0c6626b9251b02eedab997b5fc792a0de
Paul Nguyen <toan359@gmail.com>
2018-07-13 12:54:14 +0700

    1. Smoke test

---
+++ contracts/DappToken.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.24;
+
+contract DappToken {
+	// Constructor 
+	// Set the total of tokens
+	// Read the total number of tokens
+
+	uint256 public totalSupply;
+
+	function DappToken () public {
+		totalSupply = 1000000;	
+	}
+}
