9dab5e1f3e4699587c9d1cb3c47e8ad89a5b92d9
Paul Nguyen <toan359@gmail.com>
2018-07-17 14:13:57 +0700

    3. delegated Transfer

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -14,7 +14,18 @@ contract DappToken {
 		uint256 _value
 	);
 
+	event Approval(
+		address indexed _owner,
+		address indexed _spender,
+		uint256 indexed _value
+	);
+
+	// transfer
+
 	mapping(address => uint256) public balanceOf;
+	mapping(address => mapping(address => uint256)) public allowance;
+	// allowance
+
 
 	constructor  (uint256 _initialSupply) public {
 		balanceOf[msg.sender] = _initialSupply;
@@ -37,4 +48,44 @@ contract DappToken {
 
 		return true;
 	}
+
+	// approve 
+
+	function approve(address _spender, uint256 _value) public returns (bool success) {
+		// Allowance 
+		allowance[msg.sender][_spender] = _value;
+
+		// Approve event	
+		Approval(msg.sender, _spender, _value);
+
+		return true;
+	}
+
+	// transfer from 
+
+	function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+
+		require(_value <= balanceOf[_from]);
+		require(_value <= allowance[_from][msg.sender]);
+		// Require allowance is big enough
+		balanceOf[_from] -= _value;
+		balanceOf[_to] += _value;
+		
+		allowance[_from][msg.sender] -= _value;
+
+		Transfer(_from, _to, _value);
+		return true;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
 }
